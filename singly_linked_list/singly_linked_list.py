e948f91435a035e223d4984920e2613bad13ed6a
RohithSachdeva <rsachdeva@ucdavis.edu>
2021-03-08 16:46:12 -0800

    Monday Start

---
+++ singly_linked_list/singly_linked_list.py
@@ -0,0 +1,13 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+# Dealing with nodes that have a pointer
+
+"""
+Node with a pointer thats called next
+
+
+"""
